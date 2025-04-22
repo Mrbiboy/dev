89670bf36299add6e48bc8695b3ba89b2614d124
anas <tahallianas74@gmail.com>
2025-04-22 18:16:47 +0100

    scan par checkov

---
+++ backend/unzip.py
@@ -0,0 +1,9 @@
+import zipfile
+import tempfile
+import os
+
+def extract_zip(zip_path):
+    temp_dir = tempfile.mkdtemp()
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(temp_dir)
+    return temp_dir
